ddbb10337945b73b854f37fa7fdc53dd3bfe4451
Gangmin Lee <gangminlee@outlook.com>
2021-11-08 22:13:42 +0900

    python self-studying 2nd

---
+++ practice02.py
@@ -0,0 +1,19 @@
+import math
+print('math.sqrt(4)', math.sqrt(4))
+print('math.pow(4,2)', math.pow(4,2))
+
+import random
+print('random.random()', random.random())
+
+print('Hello world')
+print("Hello world")
+print('''
+Hello
+World
+''')
+
+print('1'+'1')
+
+print('Hello world '*100)
+print(len('Hello world '*100))
+print('Hello world'.replace('world','universe'))
